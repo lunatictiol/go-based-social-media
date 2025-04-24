aee32dc2d9e63ac0120c8626101eae3206828e59
lunatictiol <bhatsabiq9@gmail.com>
2025-04-24 22:17:47 +0530

    setup migration

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/joho/godotenv"
+	"github.com/lunatictiol/go-based-social-media/internal/db"
 	"github.com/lunatictiol/go-based-social-media/internal/env"
 	"github.com/lunatictiol/go-based-social-media/internal/store"
 )
@@ -13,11 +15,33 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+	maxOpenConns, err := env.GetInt("DB_MAX_OPEN_CON", 20)
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	maxIdleConns, err := env.GetInt("DB_MAX_IDLE_CON", 20)
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	cfg := config{
 		addr: env.GetString("PORT", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres"),
+			maxOpenConns: maxOpenConns,
+			maxIdleConns: maxIdleConns,
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
-	store := store.NewStorage(nil)
+	fmt.Println("connecting to database")
+	db, err := db.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer db.Close()
+	store := store.NewStorage(db)
 	app := &application{
 		config: cfg,
 		store:  store,
