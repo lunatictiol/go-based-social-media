3ad5752d8e300034726e3d0cc5d05e21ffa9fca1
lunatictiol <bhatsabiq9@gmail.com>
2025-04-30 03:32:12 +0530

    create followers logic

--- cmd/api/error.go
+++ cmd/api/error.go
@@ -23,7 +23,7 @@ func (app *application) NotfoundResponse(w http.ResponseWriter, r *http.Request,
 }
 
 func (app *application) conflictResponse(w http.ResponseWriter, r *http.Request, err error) {
-	log.Printf("not found\n Method : %s path : %s error: %s", r.Method, "path", r.URL.Path, "error", err.Error())
+	log.Printf("conflict\n Method : %s path : %s error: %s", r.Method, r.URL.Path, "error", err.Error())
 
 	WriteJSONError(w, http.StatusConflict, err.Error())
 }
